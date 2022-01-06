36b242087ba70475b9df5676f776e19171f28f26
AliceMenzie <alice.menzie@gmail.com>
2022-01-06 15:58:48 +1100

    add initial header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,33 @@
-import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
+import { Flex } from "./styles/Flex.styled";
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
 
 export default function Header() {
-    return (
-        <StyledHeader>
-            <Nav>
-                <Logo src="./images/logo.svg" alt="easybank"/>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Blog</li>
-                    <li>Careers</li>
-                </ul>
-                <button>Request Invite</button>
-            </Nav>
-            
-            
-
-
-
-
-
-            </div>
-            
-        </StyledHeader>
-    )
+  return (
+    <StyledHeader>
+      <Nav>
+        <Logo src="./images/logo.svg" alt="easybank" />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Blog</li>
+          <li>Careers</li>
+        </ul>
+        <button>Request Invite</button>
+      </Nav>
+      <Flex>
+          <div>
+        <h1>Next generation digital banking</h1>
+        <p>
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button>Request Invite</button>
+        </div>
+        <Image src="./images/bg-intro-desktop.svg" alt="mobile phones"/>
+        {/* <Image src="./images/image-mockups.png" alt="mobile phones"/> */}
+      </Flex>
+    </StyledHeader>
+  );
 }
-
-
-
-
-
-
-
-
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -2,14 +2,27 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
 
+&::before {
+            content: "";
+            position: absolute;
+            background: url("../../images/image-mockups.png");
+            background-size: 75%;
+            width: 50%;
+            height: 100%;
+            background-repeat: no-repeat;
+            background-position: right center;
+            left: 54rem;
+            bottom: 5rem;
+            z-index: -1;
 
-
+}
 `
 export const Nav = styled.nav`
     display: flex;
     align-items: center;
     justify-content: space-around;
     background-color: white;
+    z-index: 99999;
 
     ul {
         display: flex;
@@ -34,5 +47,11 @@ export const Nav = styled.nav`
 export const Logo = styled.img`
 width: 10rem;
 
+`
 
+export const Image = styled.img`
+    max-width: 50%;
+    margin-right: -12rem;
+    margin-top: -8rem;
+    z-index: -2;
 `
