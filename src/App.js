85ce86255a1ad80672770410d0da380437ebc710
AliceMenzie <alice.menzie@gmail.com>
2022-01-07 14:23:41 +1100

    add global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import GlobalStyles from "./components/styles/Global";
 
 function App() {
+  const theme = {
+    colors: {
+      greyBlue: "hsl(233, 8%, 62%)",
+      lightGreyBlue: "hsl(220, 16%, 96%)",
+      lightGrey: "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+      darkBlue: "hsl(233, 26%, 24%)",
+      limeGreen: "hsl(136, 65%, 51%)",
+      brightCyan: "hsl(192, 70%, 51%)",
+    },
+    // mobile: "768px"
+    tablet: "645px",
+    mobile: "375px",
+  };
+
   return (
-    <>
-      <GlobalStyles />
-      <Header/>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,17 +1,43 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  /* @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap'); */
-  * {
-    box-sizing: border-box;
-  }
+  @import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;700&display=swap');
+  *,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+figure,
+picture {
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  font-weight: 400;
+}
+
   body {
     overflow: hidden;
     background: white;
     color: hsl(192, 100%, 9%);
-    font-family: sans-serif;
+    font-family: 'Public Sans', sans-serif;
     font-size: 1.15em;
     margin: 0;
   }
-`
-  export default GlobalStyles
+`;
+export default GlobalStyles;
